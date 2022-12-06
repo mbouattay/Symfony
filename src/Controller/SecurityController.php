6800764e2d1b119be6698592e5518db6a421da27
mbouattay <mahmoudbouattay178@gmail.com>
2022-12-06 20:00:40 +0100

    version 5

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,35 +15,7 @@ class SecurityController extends AbstractController
     #[Route('/register', name: 'register',  methods: ['GET', 'POST'])]
     public function register(Client $client=null,Request $request,EntityManagerInterface $manager,AuthenticationUtils $authenticationUtils): Response
     {
-        if($_POST['nom']!='' && $_POST['prenom']!=''
-            && $_POST['email']!='' && $_POST['telephone']!=''
-            && $_POST['password'] == $_POST['confirm_password']
-            ){
-                $data=new Client;
-                $data->setNom($_POST['nom']);
-                $data->setPrenom($_POST['prenom'])
-                    ->setEmail($_POST['email'])    
-                    ->setPassword($_POST['password'])
-                    ->setTel($_POST['telephone'])
-                    ->setRole("client");
-                $em->persist($data);  
-                $em->flush();
-
-                // Apres inscription
-                $_POST = array();
-                unset($data);
-                
-                return $this->redirectToRoute('/');
-
-            }else{
-                echo('
-                <script>
-                    alert("Revoyez votre saisie ! ");
-                </script>
-                ');
-                $_POST = array();
-            }
-
+      
         return $this->render('security/register.html.twig', [
             'controller_name' => 'SecurityController',
         ]);
